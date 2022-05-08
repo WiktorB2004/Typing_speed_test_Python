371f2abf3941286c24359864a3a156dd527d70d3
Wiktor <byrka.w@gmail.com>
2022-05-08 13:15:47 +0200

    created app structure and imported needed modules

---
+++ main.py
@@ -0,0 +1,36 @@
+# Imports
+import random
+from curses import wrapper
+
+# Generates a random sentence using strings from wordlist
+def generate_sentence(wordlist):
+    pass
+
+
+# Display and style the content of the app on the users screen
+def display():
+    pass
+
+
+# Check if user wrote correct character
+def check_user_input():
+    pass
+
+
+# Analyze min,max and avg WPM
+def analyze_typing_speed():
+    pass
+
+
+# Displays before starting a "game" function
+def menu():
+    pass
+
+
+# Main function of the program - handles whole performance of the app
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
